82f62d7719ba30e3dda4f2152be749a84ce4c7d6
Aaron Collier <aaron.collier@stanford.edu>
2025-03-28 09:29:01 -0700

    Establish initial login test

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -2,11 +2,12 @@
 
 # Helpers to assist with authentication.
 module AuthenticationHelpers
-  def authentication_headers_for(user)
+  def authentication_headers_for(user, groups: [])
     {
-      Authentication::REMOTE_USER_HEADER => user.email_address,
-      Authentication::NAME_HEADER => user.name,
-      Authentication::FIRST_NAME_HEADER => user.first_name
+      Settings.http_headers.remote_user => user.email_address,
+      Settings.http_headers.full_name => user.name,
+      Settings.http_headers.first_name => user.first_name,
+      Settings.http_headers.user_groups => groups.join(';')
     }
   end
 
@@ -15,7 +16,17 @@ module AuthenticationHelpers
     TestShibbolethHeaders.groups = groups
   end
 
+  private
+
+  def clear_shibboleth_headers
+    TestShibbolethHeaders.user = nil
+    TestShibbolethHeaders.groups = nil
+  end
+
   RSpec.configure do |config|
+    # The class variables used in TestShibbolethHeaders can stick around between specs, so clearing.
+    config.before(:each, type: :request) { clear_shibboleth_headers }
+    config.before(:each, type: :system) { clear_shibboleth_headers }
     config.include AuthenticationHelpers, type: :system
     config.include AuthenticationHelpers, type: :request
   end
