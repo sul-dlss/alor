802c5cbf8b99d782f6003f6f3ac6bfa303545cdb
Aaron Collier <aaron.collier@stanford.edu>
2025-03-28 10:10:54 -0700

    Enable simplecov test coverage

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -35,21 +35,34 @@ Config.setup do |config|
   #
   config.env_prefix = 'SETTINGS'
 
+  # Load environment variables from the `ENV` object and override any settings defined in files.
+  #
+  config.use_env = true
+
+  # Define ENV variable prefix deciding which variables to load into config.
+  #
+  # Reading variables from ENV is case-sensitive. If you define lowercase value below, ensure your ENV variables are
+  # prefixed in the same way.
+  #
+  # When not set it defaults to `config.const_name`.
+  #
+  config.env_prefix = 'SETTINGS'
+
   # What string to use as level separator for settings loaded from ENV variables. Default value of '.' works well
   # with Heroku, but you might want to change it for example for '__' to easy override settings from command line, where
   # using dots in variable names might not be allowed (eg. Bash).
   #
-  # config.env_separator = '.'
+  config.env_separator = '__'
 
   # Ability to process variables names:
   #   * nil  - no change
   #   * :downcase - convert to lower case
   #
-  # config.env_converter = :downcase
+  config.env_converter = :downcase
 
   # Parse numeric values as integers instead of strings.
   #
-  # config.env_parse_values = true
+  config.env_parse_values = true
 
   # Validate presence and type of specific config values. Check https://github.com/dry-rb/dry-validation for details.
   #
