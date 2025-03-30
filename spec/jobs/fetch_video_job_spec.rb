016208a8f8256d5a431a448c5145c0f87b3d5e46
Aaron Collier <aaron.collier@stanford.edu>
2025-03-30 16:09:08 -0700

    Add initial fetch video job spec

---
+++ spec/jobs/fetch_video_job_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe FetchVideoJob do
+  let(:channel) { create(:channel) }
+  let(:video) { create(:video, channel:) }
+  let(:client) { Youtube::Client.new(channel_id: channel.channel_id) }
+
+  before do
+    allow(Youtube::Client).to receive(:new).with(channel_id: channel.channel_id).and_return(client)
+    allow(client).to receive(:video_data).and_return(video.data)
+    allow(client).to receive(:caption_data)
+  end
+
+  context 'when fetching data for a new video' do
+    it 'populates video data' do
+      # expect(channel.data).to eq({}) # Ensure the channel starts with no data
+      described_class.perform_now(video_id: video.video_id, client:)
+      expect(video.reload.data).to eq(video.data)
+    end
+  end
+end
